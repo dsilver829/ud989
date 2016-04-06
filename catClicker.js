0a8e3fbf8f1f07b876f6ba587b654bd6f266ca7f
David Silver <dsilver@candidatemetrics.com>
2016-04-05 20:15:07 -0700

    support multiple cats

--- catClicker.js
+++ catClicker.js
@@ -1,29 +1,46 @@
 function Cat(name) {
   this.name = name;
-  $("#names").append(this.cell(this.name));
+  this.link = $('<a href="#"></a>').text(this.name);
+  var item = $('<li></li>').html(this.link);
+
+  this.nameDiv = this.div().text(this.name);
 
   this.image = $("<img>");
   this.image.attr('src', this.name.toLowerCase() + ".jpg");
-  var cell = this.cell().append(this.image);
-  $("#images").append(cell);
   this.image.click(this.increment.bind(this));
+  this.imageDiv = this.div().html(this.image);
 
   this.count = 0;
-  this.counter = this.cell(this.count.toString());
-  $("#counters").append(this.counter);
+  this.countDiv = this.div().text(this.count);
+
+  this.cat = this.div();
+  this.cat.css("display", "none");
+  this.cat.append(this.nameDiv);
+  this.cat.append(this.imageDiv);
+  this.cat.append(this.countDiv);
+
+  this.link.click(function() {
+    $('#cat').children().css("display", "none");
+    this.cat.css("display", "");
+  }.bind(this));
+  
+  $('#names').append(item);
+  $('#cat').append(this.cat);
 };
 
 Cat.prototype = {
-  cell: function(text) {
-    return $('<td></td>').text(text);
+  div: function() {
+    return $('<div></div>');
   },
   increment: function() {
     this.count += 1;
-    this.counter.text(this.count.toString());
+    this.countDiv.text(this.count.toString());
   }
 };
 
 $(document).ready(function() {
-  new Cat("Cat");
-  new Cat("Chewie");
+  cats = ["Cat", "Chewie", "Jetske", "Tom", "Jerry"];
+  for(var i = 0; i < cats.length; i++) {
+    new Cat(cats[i]);
+  }
 });
