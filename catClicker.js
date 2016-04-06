5948ea89cad744bd651a3d11db18d289151efe39
David Silver <dsilver@candidatemetrics.com>
2016-04-05 19:12:59 -0700

    move more logic into the JavaScript class

--- catClicker.js
+++ catClicker.js
@@ -1,15 +1,28 @@
-function Counter() {
-  count = 0;
-  $('img').click(this.increment);
-}
+function Cat(name) {
+  this.name = name;
+  $("#names").append(this.cell(this.name));
 
-Counter.prototype = {
-  increment: function() {
-    count += 1;
-    $('#counter').text(count.toString());
-  } 
-}
+  this.image = $("<img>");
+  this.image.attr('src', this.name.toLowerCase() + ".jpg");
+  var cell = this.cell().append(this.image);
+  $("#images").append(cell);
+  this.image.click(this.increment.bind(this));
+
+  this.count = 0;
+  this.counter = this.cell(this.count.toString());
+  $("#counters").append(this.counter);
+};
+
+Cat.prototype.cell = function(text) {
+  return $('<td></td>').text(text);
+};
+
+Cat.prototype.increment = function() {
+  this.count += 1;
+  this.counter.text(this.count.toString());
+};
 
 $(document).ready(function() {
-  var counter = new Counter();
+  'use strict';
+  var cat = new Cat("Cat");
 });
